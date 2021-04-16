6df54f16d0fdb50bdb18a312067ae09329cad41e
hemanta212 <sharmahemanta.212@gmail.com>
2021-04-16 15:49:25 +0545

    Integrate frontend and backend withe react axios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,26 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 import './App.css';
 
-const list = [ { "id":1,
-"title":"1st todo",
-"body":"Learn Django properly."
-},
-{
-"id":2,
-"title":"Second item",
-"body":"Learn Python."
-},
-]
 
 class App extends Component {
-    constructor(props) {
-	super(props);
-	this.state = { list };
+    state = { todos: [] };
+    componentDidMount() {
+	this.getTodos();
+    }
+    getTodos() {
+    axios
+	.get('http://127.0.0.1:8000/api/')
+	.then(res => {
+	    this.setState({ todos: res.data });
+	})
+	.catch(err => {
+	    console.log(err);
+	});
     }
     render() {
-	return ( <div> {this.state.list.map(item => ( <div key={item.id}> <h1>{item.title}</h1> <p>{item.body}</p> </div> ))} </div> );
+	return ( <div> {this.state.todos.map(item => ( <div key={item.id}> <h1>{item.title}</h1> <p>{item.body}</p> </div> ))} </div> );
     }
 }
 
