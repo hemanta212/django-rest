b8baf3d4cba8615c0581766187926f9222c5462f
hemanta212 <sharmahemanta.212@gmail.com>
2021-04-17 15:34:06 +0545

    Adds token authentication and user registration

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('posts.urls')),
-    path('api-auth', include('rest_framework.urls')),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/v1/user-auth/', include('dj_rest_auth.urls')),
+    path('api/v1/user-auth/registration', include('dj_rest_auth.registration.urls')),
 ]
