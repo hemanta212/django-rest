89b9ba095f66dab867d6e802ed38d0e4a272602d
hemanta212 <sharmahemanta.212@gmail.com>
2021-04-17 09:44:50 +0545

    Create custom permission and set proj-level permissions

--- posts/views.py
+++ posts/views.py
@@ -1,15 +1,15 @@
 from rest_framework import generics, permissions
+from .permissions import IsAuthorOrReadOnly
 from .models import Post
 from .serializers import PostSerializer
 
 
-class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-    permission_classes = (permissions.IsAuthenticated, )
-
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = (permissions.IsAuthenticated, )
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (IsAuthorOrReadOnly, )
 
