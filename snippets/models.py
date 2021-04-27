ad2cfa323ba77cd90279966a0f37953837b62c11
hemanta212 <sharmahemanta.212@gmail.com>
2021-04-27 11:27:38 +0000

    Wip: add owner and highlight save hook

--- snippets/models.py
+++ snippets/models.py
@@ -1,5 +1,7 @@
 from django.db import models
-from pygments.lexers import get_all_lexers
+from pygments import highlight
+from pygments.formatters.html import HtmlFormatter
+from pygments.lexers import get_all_lexers, get_lexer_by_name
 from pygments.styles import get_all_styles
 
 
@@ -7,7 +9,9 @@ LEXERS = [item for item in get_all_lexers() if item[1]]
 # Get_all_lexers returns in format (name, (alias,), ....) we just want name & first alias
 lang_name, alias, first_alias = 0, 1, 0
 # choices -> list[tuple] -> tuple[0] -> actual value, tuple[1] -> human readable name
-LANGUAGE_CHOICES = sorted([(item[alias][first_alias], item[lang_name]) for item in LEXERS])
+LANGUAGE_CHOICES = sorted(
+    [(item[alias][first_alias], item[lang_name]) for item in LEXERS]
+)
 STYLE_CHOICES = sorted([(item, item) for item in get_all_styles() if item])
 
 
@@ -20,9 +24,26 @@ class Snippet(models.Model):
         choices=LANGUAGE_CHOICES, default="python", max_length=100
     )
     style = models.CharField(choices=STYLE_CHOICES, default="friendly", max_length=100)
+    owner = models.ForeignKey('auth.User', related_name='snippet', on_delete=models.CASCADE)
+    highlighted = models.TextField(default='')
+
 
     class Meta:
         ordering = ["created"]
 
+    def save(self, *args, **kwargs):
+        """
+        Use the `pygments` library to create a highlighted HTML
+        representation of the code snippet.
+        """
+        lexer = get_lexer_by_name(self.language)
+        lineos = 'table' if self.lineos else False
+        options = {'title': self.title} if self.title else {}
+        formatter = HtmlFormatter(style=self.style, lineos=lineos,
+                                  full=True, **options)
+        self.highlighted = highlight(self.code, lexer, formatter)
+        super(Snippet, self).save(*args, **kwargs)
+      
     def __str__(self):
         return self.title
+
